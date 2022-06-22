1a401d55d4813f4d1ff923bdb30bee78a97d7f22
wagner <wagncarv@outlook.com>
2022-06-22 19:54:03 -0300

    Refatoração função build

--- lib/orders/order.ex
+++ lib/orders/order.ex
@@ -1,15 +1,31 @@
 defmodule Exlivery.Orders.Order do
+  alias Exlivery.Orders.Item
+  alias Exlivery.Users.User
+
   @keys [:user_cpf, :delivery_address, :items, :total_price]
   @enforce_keys @keys
   defstruct @keys
 
-  def build() do
+  def build(%User{cpf: cpf, address: address}, [%Item{} | _items] = items) do
     {:ok,
      %__MODULE__{
-       user_cpf: :user_cpf,
-       delivery_address: :delivery_address,
-       items: :items,
-       total_price: :total_price
+       user_cpf: cpf,
+       delivery_address: address,
+       items: items,
+       total_price: calculate_total_price(items)
      }}
+  end
+
+  def build(_user, _items), do: {:error, "invalid parameters"}
+
+  defp calculate_total_price(items) do
+    items
+    |> Enum.reduce(Decimal.new("0.00"), &sum_prices(&1, &2))
+  end
+
+  defp sum_prices(%Item{unity_price: price, quantity: quantity}, acc) do
+    price
+    |> Decimal.mult(quantity)
+    |> Decimal.add(acc)
   end
 end
