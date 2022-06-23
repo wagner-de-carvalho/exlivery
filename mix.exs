1597daad1669ddbc488ceb5ba15111a64b053049
wagner <wagncarv@outlook.com>
2022-06-23 11:09:40 -0300

    Config

--- mix.exs
+++ mix.exs
@@ -6,10 +6,16 @@ defmodule Exlivery.MixProject do
       app: :exlivery,
       version: "0.1.0",
       elixir: "~> 1.13",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
+
+  # This makes sure your factory and any other modules in test/support are compiled
+  # when in the test environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
@@ -22,7 +28,8 @@ defmodule Exlivery.MixProject do
   defp deps do
     [
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:decimal, "~> 2.0"}
+      {:decimal, "~> 2.0"},
+      {:ex_machina, "~> 2.7.0"}
     ]
   end
 end
