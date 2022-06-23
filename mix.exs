defef74d40936734f41fad2da5ba31203eb4b19a
wagner <wagncarv@outlook.com>
2022-06-23 19:47:50 -0300

    Biblioteca uuid

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Exlivery.MixProject do
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:decimal, "~> 2.0"},
       {:ex_machina, "~> 2.7.0"},
-      { :uuid, "~> 1.1" }
+      {:uuid, "~> 1.1"}
     ]
   end
 end
