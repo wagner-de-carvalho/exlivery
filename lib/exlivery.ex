9b527f9cd12d3b0ba8d0949096aa71fc23cb2e1a
wagner <wagncarv@outlook.com>
2022-06-23 16:34:21 -0300

    Fachada atualizada

--- lib/exlivery.ex
+++ lib/exlivery.ex
@@ -1,9 +1,11 @@
 defmodule Exlivery do
+  alias Exlivery.Orders.Agent, as: OrderAgent
   alias Exlivery.Users.Agent, as: UserAgent
   alias Exlivery.Users.CreateOrUpdate
 
   def start_agents do
     UserAgent.start_link(%{})
+    OrderAgent.start_link(%{})
   end
 
   defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
