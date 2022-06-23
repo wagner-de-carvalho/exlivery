2715270ad5a83c2381edeac31ced545463d8c40c
wagner <wagncarv@outlook.com>
2022-06-23 16:53:46 -0300

    Create or update order

--- lib/orders/create_or_update.ex
+++ lib/orders/create_or_update.ex
@@ -1,3 +1,17 @@
 defmodule Exlivery.Orders.CreateOrUpdate do
+  alias Exlivery.Orders.Agent, as: OrderAgent
   alias Exlivery.Orders.Order
+  alias Exlivery.Users.Agent, as: UserAgent
+
+  def call(%{user_cpf: cpf, items: items}) do
+    with {:ok, user} <- UserAgent.get(cpf),
+         {:ok, items} <- build_items(items),
+         {:ok, order} <- Order.build(user, items) do
+      OrderAgent.save(order)
+    end
+  end
+
+  defp build_items(items) do
+    {:ok, items}
+  end
 end
