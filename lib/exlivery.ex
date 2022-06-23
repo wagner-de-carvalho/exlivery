91755cacf73c286842ca51fac7134ea910ac9456
wagner <wagncarv@outlook.com>
2022-06-23 15:55:48 -0300

    Fachada para criar user

--- lib/exlivery.ex
+++ lib/exlivery.ex
@@ -1,3 +1,10 @@
 defmodule Exlivery do
-  defstruct [:name, :email]
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Users.CreateOrUpdate
+
+  def start_agents do
+    UserAgent.start_link(%{})
+  end
+
+  defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
 end
