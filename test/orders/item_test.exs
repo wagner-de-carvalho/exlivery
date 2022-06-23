5e13767a1a8a7f1e25659a17f370538d325ce702
wagner <wagncarv@outlook.com>
2022-06-23 13:08:23 -0300

    Testes módulo item

--- test/orders/item_test.exs
+++ test/orders/item_test.exs
@@ -1,5 +1,39 @@
 defmodule Exlivery.Orders.ItemTest do
   use ExUnit.Case
+  alias Exlivery.Orders.Item
+  import Exlivery.Factory
 
-  
+  describe "build/4" do
+    test "when all params are valid, returns an item" do
+      response = Item.build("Pizza de calabresa", :pizza, "42.50", 1)
+
+      expected_response = {:ok, build(:item)}
+
+      assert response == expected_response
+    end
+
+    test "when there is an invalid category, returns an error" do
+      response = Item.build("Pizza de calabresa", :invalid, "42.50", 1)
+
+      expected_response = {:error, "Invalid parameters"}
+
+      assert response == expected_response
+    end
+
+    test "when there is an invalid price, returns an error" do
+      response = Item.build("Pizza de calabresa", :pizza, "AAA", 1)
+
+      expected_response = {:error, "Invalid price"}
+
+      assert response == expected_response
+    end
+
+    test "when there is an invalid quantity, returns an error" do
+      response = Item.build("Pizza de calabresa", :pizza, "42.50", 0)
+
+      expected_response = {:error, "Invalid parameters"}
+
+      assert response == expected_response
+    end
+  end
 end
