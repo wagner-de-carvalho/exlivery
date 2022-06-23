6509e257cb9b8cbcbeba84600b8eed0173b1c4a2
wagner <wagncarv@outlook.com>
2022-06-23 19:09:38 -0300

    Create or update order testes

--- test/users/create_or_update_test.exs
+++ test/users/create_or_update_test.exs
@@ -21,7 +21,6 @@ defmodule Exlivery.Users.CreateOrUpdateTest do
     test "when all params are valid, saves the user", %{user_params: user_params} do
       response = CreateOrUpdate.call(user_params)
       expected_response = {:ok, "User created or updated successfully"}
-      
       assert response == expected_response
     end
 
