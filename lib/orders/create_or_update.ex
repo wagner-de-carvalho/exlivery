367e925681294f14c0c6445e3a9573c57eef4343
wagner <wagncarv@outlook.com>
2022-06-23 18:03:34 -0300

    Create or update user

--- lib/orders/create_or_update.ex
+++ lib/orders/create_or_update.ex
@@ -1,6 +1,6 @@
 defmodule Exlivery.Orders.CreateOrUpdate do
   alias Exlivery.Orders.Agent, as: OrderAgent
-  alias Exlivery.Orders.Order
+  alias Exlivery.Orders.{Item, Order}
   alias Exlivery.Users.Agent, as: UserAgent
 
   def call(%{user_cpf: cpf, items: items}) do
@@ -12,6 +12,24 @@ defmodule Exlivery.Orders.CreateOrUpdate do
   end
 
   defp build_items(items) do
-    {:ok, items}
+    items
+    |> Enum.map(&build_item/1)
+    |> handle_build()
+  end
+
+  defp build_item(%{
+         description: description,
+         category: category,
+         unity_price: unity_price,
+         quantity: quantity
+       }) do
+    case Item.build(description, category, unity_price, quantity) do
+      {:ok, item} -> item
+      {:error, _reason} = error -> error
+    end
+  end
+
+  defp handle_build(items) do
+    if Enum.all?(items, &is_struct/1), do: {:ok, items}, else: {:error, "Invalid items"}
   end
 end
